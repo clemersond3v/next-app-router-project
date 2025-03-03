fff0e90714aa0063174473333e5a71aef08f8dde
Clemerson Costa <clemerson.costa.vendor@asics.com>
2025-03-03 12:40:03 -0300

    fix: resolve deploy automation error

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//         pathname: '/dkh8odl7j/**', // Substitua pelo seu path específico do Cloudinary, se aplicável
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/dkh8odl7j/**', // Substitua pelo seu path específico do Cloudinary, se aplicável
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
+
