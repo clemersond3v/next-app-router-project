9bc7ac29bd051b912c242d5583a7b13d3346af7f
Clemerson Costa <clemerson.costa.vendor@asics.com>
2025-03-05 00:51:33 -0300

    fix: webpack advanced config

--- next.config.ts
+++ next.config.ts
@@ -16,41 +16,7 @@
 
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack(config, { isServer }) {
-    // Habilita o Code Splitting
-    config.optimization.splitChunks = {
-      chunks: "all",
-      minSize: 20000, // Mínimo de 20KB para um novo chunk
-      maxSize: 250000, // Máximo de 250KB para evitar arquivos grandes
-      maxInitialRequests: 10, // Número máximo de requests simultâneos
-      cacheGroups: {
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          priority: 10,
-          enforce: true,
-        },
-        commons: {
-          test: /[\\/]src[\\/]components[\\/]/,
-          name: "commons",
-          minChunks: 2,
-          priority: 5,
-          enforce: true,
-        },
-      },
-    };
-
-    // Configuração específica para otimizar build no cliente e servidor
-    if (!isServer) {
-      config.optimization.runtimeChunk = "single";
-    }
-
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
-
-
 
